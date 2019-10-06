d2e397ffa6efe07fadb1dfb41139e27003055f22
Barry Becker <barrybecker4@gmail.com>
2019-10-05 19:48:29 -0700

    Upgrade to java 11 scala 2.13

--- scala-source/com/barrybecker4/newpackage/HelloWorld.scala
+++ scala-source/com/barrybecker4/newpackage/HelloWorld.scala
@@ -1,4 +1,4 @@
-// Copyright by Barry G. Becker, 2017. Licensed under MIT License: http://www.opensource.org/licenses/MIT
+// Copyright by Barry G. Becker, 2019. Licensed under MIT License: http://www.opensource.org/licenses/MIT
 package com.barrybecker4.newpackage
 
 object HelloWorld extends App {
