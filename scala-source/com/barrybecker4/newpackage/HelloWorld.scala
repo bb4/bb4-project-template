68bd54fde3a5c16b8dfa17b963cb4bd3516c596c
Barry Becker <barrybecker4@gmail.com>
2023-05-14 05:23:49 -0700

    Update to java 19 and gradle 7.3

--- scala-source/com/barrybecker4/newpackage/HelloWorld.scala
+++ scala-source/com/barrybecker4/newpackage/HelloWorld.scala
@@ -1,4 +1,4 @@
-// Copyright by Barry G. Becker, 2019. Licensed under MIT License: http://www.opensource.org/licenses/MIT
+// Copyright by Barry G. Becker, 2023. Licensed under MIT License: http://www.opensource.org/licenses/MIT
 package com.barrybecker4.newpackage
 
 object HelloWorld extends App {
